71d18005e0d88a26f1bc0b78669c83d96f8a9d1e
Tan Quang DUONG <tquangbk@gmail.com>
2024-01-29 22:53:40 +0100

    update ai chatbot

---
+++ embedding/embedding_pgvector.py
@@ -0,0 +1,43 @@
+import os
+from dotenv import load_dotenv
+from langchain.vectorstores.pgvector import PGVector
+from langchain_openai import OpenAIEmbeddings
+from langchain.text_splitter import RecursiveCharacterTextSplitter
+from utils import file_loader
+
+# Define the directory where the data source files are located
+DATASOURCE_DIR = "./datasource/"
+
+CONNECTION_STRING = (
+    "postgresql+psycopg2://postgres:123456@localhost:5432/doctor_vector_db"
+)
+COLLECTION_NAME = "doctor_lib_vectors"
+
+
+def process_embedding():
+    embeddings = OpenAIEmbeddings()
+    loaders = []
+    # Iterate over all files in the directory
+    for f in os.listdir(DATASOURCE_DIR):
+        # If the current item is a file
+        file_path = os.path.join(DATASOURCE_DIR, f)
+        if os.path.isfile(file_path):
+            loaders.append(file_loader(file_path))
+    docs = []
+    for loader in loaders:
+        docs.extend(loader.load())
+
+    # This text splitter is used to create the child documents
+    child_splitter = RecursiveCharacterTextSplitter(chunk_size=400)
+    texts = child_splitter.split_documents(docs)
+
+    db = PGVector.from_documents(
+        embedding=embeddings,
+        documents=texts,
+        collection_name=COLLECTION_NAME,
+        connection_string=CONNECTION_STRING,
+    )
+
+
+if __name__ == "__main__":
+    process_embedding()
